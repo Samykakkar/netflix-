aa613aeccb4f7f98b1b12b109096d0836f73a10c
Sameeer Jain <sam8377984375@gmail.com>
2021-07-01 13:48:20 +0530

    Homepage netflix

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Container, Inner, Pane,Item, Title, SubTitle, Image } from './styles/jumbotron'
+
+export default function Jumbotron({ children, direction = 'row', ...restprops }) {
+    return (
+        <Item direction={direction}>
+            <Inner >
+                {children}
+            </Inner>
+        </Item>
+    )
+}
+
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restprops }) {
+    return <Container{...restprops}>{children}</Container>
+}
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restprops }) {
+    return <Pane{...restprops}>{children}</Pane>
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restprops }) {
+    return <Title{...restprops}>{children}</Title>
+}
+
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restprops }) {
+    return <SubTitle{...restprops}>{children}</SubTitle>
+}
+
+Jumbotron.Image = function JumbotronImage({ children, ...restprops }) {
+    return <Image{...restprops} />
+}
