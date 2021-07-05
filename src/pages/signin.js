5b7b759b9d4cb6d1fb5f959ddfee8e4ebbf9fa09
Sameeer Jain <sam8377984375@gmail.com>
2021-07-05 13:03:29 +0530

    Sigin

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { isValidElement, useState } from "react";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import { Form } from "../components";
 
-export default function Signin(){
-    return (
-        <p>Hello from the signin</p>
-    )
-}; 
+export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState("");
+ 
+  const isInvalid =password =='' || emailAddress == '';
+   const handleSignIn =(event) => {
+       event.preventDefault();
+   }
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onchange={({ target }) => setEmailAddress(target.value)}
+            />
+
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onchange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+                Sign in
+            </Form.Submit>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
+}
